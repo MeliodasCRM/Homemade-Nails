1e2e9aa946661582293064c2ed7bcab8374cb02e
MeliodasCRM <pabermb@gmail.com>
2025-03-16 22:24:09 +0000

    Correcciones en rutas

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
 
 # from models import Person
 
@@ -70,3 +71,7 @@ def serve_any_other_file(path):
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+
+# JWT CONFIG
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
